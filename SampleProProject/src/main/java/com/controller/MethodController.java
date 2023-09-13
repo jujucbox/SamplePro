4dc6d07586286c9ef2af976905528db88f95c119
jujucbox <bigjuju099@gmail.com>
2023-09-13 12:26:02 +0900

    MethodController update

--- SampleProProject/src/main/java/com/controller/MethodController.java
+++ SampleProProject/src/main/java/com/controller/MethodController.java
@@ -3,5 +3,7 @@ package com.controller;
 public class MethodController {
 	public static void main(String[] args) {
 		System.out.println("main");
+		System.out.println("Main MethodController");
+		
 	}
 }
